f503a662eb936c90379bc3457b88942e2e42d517
Umar565 <titaniumgamingr@gmail.com>
2025-02-23 23:03:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -313,7 +313,7 @@ async def account_login(bot: Client, m: Message):
                 print(f"\nError: {e}")
         return file_to_download
 
-   def vision_m3u8_link(link, Q):
+    def vision_m3u8_link(link, Q):
     Q = str(Q)
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
